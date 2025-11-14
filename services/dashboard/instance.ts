19735bde1e2f6c258b077319a4d1fa345ac221d7
Saltuk <saltuk@Saltuks-MacBook-Air.local>
2025-11-14 15:39:24 +1000

    add environment variable

--- services/dashboard/instance.ts
+++ services/dashboard/instance.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: `http://localhost:3005/api/dashboard/`,
+  baseURL: `${process.env.NEXT_PUBLIC_API_URL}/api/dashboard/`,
 });
 
 export default instance;
